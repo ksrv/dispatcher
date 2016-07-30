de54156d53e02b2832ac3b27f93ff1258a7b51f7
konstantin <peredaj@bk.ru>
2016-07-31 02:34:07 +0700

    fix

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ksrv:dispatcher',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'Simple event dispatcher.',
   git: 'git@github.com:ksrv/dispatcher.git',
   documentation: 'README.md'
@@ -13,11 +13,4 @@ Package.onUse(function(api) {
   api.use('mongo@1.1.9');
   api.export('Dispatcher');
   api.mainModule('dispatcher.js');
-});
-
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('ksrv:dispatcher');
-  api.mainModule('dispatcher-tests.js');
 });
